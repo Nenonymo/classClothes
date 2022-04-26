8eb0586526c995840ab02163214531f13d7a1916
Nenonymo <nchentre666@gmail.com>
2022-04-26 13:23:55 +0200

    cleaning input files for preprocessing

--- csv_reshape.py
+++ csv_reshape.py
@@ -1,10 +1,14 @@
 import os
 
-with open("export_dataframe.csv", mode='r') as f:
+with open("src/python/export_data_working.csv", mode='r') as f:
     data = f.readlines()
 
-with open("clean_export.csv", 'w') as fout:
+with open("clean_export.dat", 'w') as fout:
     fout.write("{}\n".format(len(data)-1))
+    lineP = ""
     for line in data[1:]:
-        clLine = line.split(",")
-        fout.write("{} {} {} {} {} {} {} {} {}\n".format(clLine[0],clLine[1][3:],clLine[2][1:],clLine[3][1:],clLine[4][1:],clLine[5][1:],clLine[6][1:],clLine[7][1:],clLine[8][1:-4]))
+        cLine = line.split(",")
+        lineP = cLine[0]
+        lineP = "{} {} {} {} {}".format(lineP, int(float(cLine[1][3:])), int(float(cLine[2][1:])), int(float(cLine[3][1:])), int(float(cLine[4][1:])))
+        lineP = "{} {} {} {} {}\n".format(lineP, int(float(cLine[5][1:])), int(float(cLine[6][1:])), int(float(cLine[7][1:])), int(float(cLine[8][1:-4])))
+        fout.write(lineP)
