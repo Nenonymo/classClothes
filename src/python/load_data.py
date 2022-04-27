74d5c628ff3a960ee9f2da6fa12c236cdfef772b
RatInMyHat <arvidb.527@gmail.com>
2022-04-27 16:05:15 +0200

    Added support for prefixes

--- src/python/load_data.py
+++ src/python/load_data.py
@@ -1,7 +1,8 @@
 import csv
 import cv2
 CSVPATH = r"../../data/csv/export_labels_working.csv"
-IMAGEPATH = r"../../data/Datasets_Enhancy/datasets/"
+IMAGEPATH = r"../../data/Datasets_Enhancy/processed/"
+IMAGEPREFIX = [ r"crop_" ]
 
 def label_cleaner(l):
     labels = l[1:len(l)-1].split(",")
@@ -24,10 +25,21 @@ def load_data():
         headings = next(csvreader)
         for row in csvreader:
             if i == 200: break # TODO debug
-            labels.append(label_cleaner(row[1]))
-            img = cv2.imread(IMAGEPATH + row[0])
-            images.append(img)
-            i += 1
+
+            # Prepare the path for a prefix
+            img_id = row[0]
+            img_id_split = img_id.split('/')
+            base_path = '/'.join([x for x in img_id_split[:-1]]) + '/'
+            img_name = img_id_split[-1]
+
+            for pre in IMAGEPREFIX:
+                # Image labels
+                labels.append(label_cleaner(row[1]))
+                # Image file
+                img = cv2.imread(IMAGEPATH + base_path + pre + img_name)
+                img = img[:,:,::-1] # BGR -> RGB
+                images.append(img)
+                i += 1
 
     return images, labels
 
