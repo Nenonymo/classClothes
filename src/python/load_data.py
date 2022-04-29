2fe3549f09dc860fd00fc50553aa6dac02a860c6
Jakob Lundkvist <jakoblundkvist3@gmail.com>
2022-04-29 16:22:12 +0200

    added parameters to load_data

--- src/python/load_data.py
+++ src/python/load_data.py
@@ -1,10 +1,10 @@
 import csv
 import cv2
-CSVPATH = r"data/csv/export_labels_working.csv"
-IMAGEPATH = r"data/Datasets_Enhancy/processed/"
-IMAGEPREFIX =  [r"crop__", r"gray__", r"wav1__", r"wav2__"] 
+from operator import itemgetter
 
-def label_cleaner(l):
+
+
+def label_cleaner(l, labelFilter):
     labels = l[1:len(l)-1].split(",")
     ret = []
     for label in labels:
@@ -13,25 +13,34 @@ def label_cleaner(l):
             label = label.replace(" ", "")
             label = label.replace("[", "")
             label = label.replace("]", "")
+            if len(labelFilter) > 0: label = itemgetter(*labelFilter)(label)
             ret.append(label)
     return ret
 
-def load_data():
+def load_data(count=-1, img_type=[], labelFilter=[]):
+    CSVPATH = r"data/csv/export_labels_working.csv"
+    IMAGEPATH = r"data/Datasets_Enhancy/processed/"
+    IMAGEPREFIX =  [r"crop__", r"gray__", r"wav1__", r"wav2__"] 
     images = []
     labels = []
+    loops = 0
+    if len(img_type) > 0: IMAGEPREFIX = img_type
     with open(CSVPATH) as csvfile:
         csvreader = csv.reader(csvfile)
         headings = next(csvreader)
         for row in csvreader:
+            if loops == count: break
             # Prepare the path for a prefix
             img_paths = [i + row[2] + ".jpg" for i in IMAGEPREFIX]
             for i in img_paths:
                 # Image labels
-                labels.append(label_cleaner(row[1]))
+                labels.append(label_cleaner(row[1], labelFilter))
                 # Image file
                 img = cv2.imread(IMAGEPATH + i)
+                print(IMAGEPATH + i)
                 img = img[:,:,::-1] # BGR -> RGB
                 images.append(img)
+            loops += 1
 
     return images, labels
 
