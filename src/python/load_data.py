9d42b208c3e4ef14a04b9673c1507ce98ee97252
Jakob Lundkvist <jakoblundkvist3@gmail.com>
2022-04-29 16:00:35 +0200

    load_data works with new file structure

--- src/python/load_data.py
+++ src/python/load_data.py
@@ -1,8 +1,8 @@
 import csv
 import cv2
-CSVPATH = r"../../data/csv/export_labels_working.csv"
-IMAGEPATH = r"../../data/Datasets_Enhancy/processed/"
-IMAGEPREFIX = [ r"crop__" ]
+CSVPATH = r"data/csv/export_labels_working.csv"
+IMAGEPATH = r"data/Datasets_Enhancy/processed/"
+IMAGEPREFIX =  [r"crop__", r"gray__", r"wav1__", r"wav2__"] 
 
 def label_cleaner(l):
     labels = l[1:len(l)-1].split(",")
@@ -17,29 +17,21 @@ def label_cleaner(l):
     return ret
 
 def load_data():
-    i = 0
     images = []
     labels = []
     with open(CSVPATH) as csvfile:
         csvreader = csv.reader(csvfile)
         headings = next(csvreader)
         for row in csvreader:
-            if i == 200: break # TODO debug
-
             # Prepare the path for a prefix
-            img_id = row[0]
-            img_id_split = img_id.split('/')
-            base_path = '/'.join([x for x in img_id_split[:-1]]) + '/'
-            img_name = img_id_split[-1]
-
-            for pre in IMAGEPREFIX:
+            img_paths = [i + row[2] + ".jpg" for i in IMAGEPREFIX]
+            for i in img_paths:
                 # Image labels
                 labels.append(label_cleaner(row[1]))
                 # Image file
-                img = cv2.imread(IMAGEPATH + pre + img_name)
+                img = cv2.imread(IMAGEPATH + i)
                 img = img[:,:,::-1] # BGR -> RGB
                 images.append(img)
-                i += 1
 
     return images, labels
 
